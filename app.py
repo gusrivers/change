642a9074ee3a17ea5199e1b2f96532d77a0b60ab
gus <gutiiz_tavuu@hotmail.com>
2024-09-19 23:49:15 -0300

    t

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.strptime)
 
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
