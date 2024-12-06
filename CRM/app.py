c6128bd2e0a2cfca9ea10018332d8d6a08f1968c
gus <gutiiz_tavuu@hotmail.com>
2024-12-06 01:19:44 -0300

    te

--- CRM/app.py
+++ CRM/app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, jsonify, request
-from flask_socketio import SocketIO, emit
 import pymysql
 import os
-import requests
-import mysql
 
 app = Flask(__name__)
 
@@ -13,19 +10,34 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'crm'
 
+# Função para criar uma conexão com o banco de dados
+def get_db_connection():
+    return pymysql.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        database=app.config['MYSQL_DB']
+    )
+
 @app.route('/')
 def index():
-    cursor = mysql.connection.cursor()
+    connection = get_db_connection()
+    cursor = connection.cursor()
     cursor.execute("SELECT id, nome, ultima_mensagem, ultima_mensagem_tempo FROM clientes")
     clientes = cursor.fetchall()
+
+    connection.close()  # Feche a conexão após o uso
 
     return render_template('index.html', clientes=clientes)
 
 @app.route('/mensagens/<int:cliente_id>', methods=['GET'])
 def mensagens(cliente_id):
-    cursor = mysql.connection.cursor()
+    connection = get_db_connection()
+    cursor = connection.cursor()
     cursor.execute("SELECT texto, tipo FROM mensagens WHERE cliente_id = %s ORDER BY data_envio", (cliente_id,))
     mensagens = cursor.fetchall()
+
+    connection.close()  # Feche a conexão após o uso
 
     return jsonify({'mensagens': [{'texto': msg[0], 'tipo': msg[1]} for msg in mensagens]})
 
@@ -35,12 +47,14 @@ def enviar_mensagem():
     cliente_id = data['cliente_id']
     texto = data['texto']
 
-    cursor = mysql.connection.cursor()
+    connection = get_db_connection()
+    cursor = connection.cursor()
     cursor.execute("INSERT INTO mensagens (cliente_id, texto, tipo) VALUES (%s, %s, 'usuario')", (cliente_id, texto))
-    mysql.connection.commit()
+    connection.commit()
+
+    connection.close()  # Feche a conexão após o uso
 
     return jsonify({'status': 'success'})
-
 
 if __name__ == '__main__':
     app.run(debug=True)
